738600a7e8878f64f5352202478759c0c5af981b
archimisha <archimisha@mail.ru>
2021-02-16 23:48:09 +0300

    done

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/Car.java
@@ -0,0 +1,47 @@
+package web.controller;
+
+public class Car {
+
+    private int count;
+    private String model;
+    private String color;
+
+    public Car(int count, String model, String color) {
+        this.count = count;
+        this.model = model;
+        this.color = color;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "var " + count +
+                ", model '" + model + '\'' +
+                ", color '" + color + '\'' +
+                '}';
+    }
+}
